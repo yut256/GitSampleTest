57c2d99732a9a94d8f9a4c115721dd3e935b5f9b
Yachen <yachentsw@gmail.com>
2022-05-31 16:22:15 +0800

    3rd Version

--- GitSample/src/TestApp/MainApp.java
+++ GitSample/src/TestApp/MainApp.java
@@ -5,6 +5,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("2nd Version");
+		// 我是組員，我要修改檔案
+		System.out.println("3rd Version");
 	}
 
 }
